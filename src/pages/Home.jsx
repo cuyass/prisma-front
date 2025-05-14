bca181296b321100a9fdea6868770a1fb6f1e701
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-14 15:58:52 +0200

    hotfix(home): change package json to fix daisyUI dependency, delete navbar from home because it is already in app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,10 @@ import HeroSection from "../components/Hero";
 function Home() {
   return (
     <div>
+        <div className="hero bg-base-200 h-full border-2 border-red-500">
+
       <HeroSection />
+      </div>
     </div>
   );
 }
