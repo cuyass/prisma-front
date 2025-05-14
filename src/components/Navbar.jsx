fbb1388868358c35d104e3e5d1f986ab4c5bb843
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-14 16:31:39 +0200

    hotfix(navbar): comment unused routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,12 +19,12 @@ const Navbar = () => {
                 tabIndex={0}
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                 <li><Link to="/">Inici</Link></li>
-                <li><Link to="/learn">Aprendre</Link></li>
+                {/* <li><Link to="/learn">Aprendre</Link></li>
                 <li><Link to="/FAQ">FAQ</Link></li>
                 <li><Link to="/directory">Directori</Link></li>
                 <li><Link to="/register">Registrar-se</Link></li>
                 <li><Link to="/login">Iniciar sessió</Link></li>
-                <li><Link to="/admindashboard">Panell d'administrador</Link></li>
+                <li><Link to="/admindashboard">Panell d'administrador</Link></li> */}
             </ul>
             </div>
         </div>
