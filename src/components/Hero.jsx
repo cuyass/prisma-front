60ce7d1f2b4b492133ef2aaa1adcf94346faa230
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-15 18:28:23 +0200

    style(hero): add style to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,10 @@ const HeroSection = () => {
     <div className="hero bg-base-200 h-full">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">
+          <h1 className="text-5xl font-bold font-[Roboto]">
             Benvingudis a PRISMA Defense Hub
           </h1>
-          <h2 className="py-6 text-xl">
+          <h2 className="py-6 text-xl font-[Roboto]">
             Seguretat digital per la comunitat: perquè les identitats són per celebrar-les, no per filtrar-les.
           </h2>
           {/* <Link to="/explore">
@@ -17,7 +17,7 @@ const HeroSection = () => {
           <Link to="/events/create">
             <button className="btn btn-secondary">Create event</button>
           </Link> */}
-          <p className="py-6">
+          <p className="py-6 font-[Roboto]">
             PRISMA és un hub de defensa digital creat per la comunitat, per a la comunitat. La nostra missió és proporcionar recursos i suport a aquells que busquen millorar la seva seguretat digital i protegir les seves identitats en línia.
           </p>
         </div>
