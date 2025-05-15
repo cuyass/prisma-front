d440ca2cbcb578a45598d7be238530b6f142c6ba
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-15 17:35:48 +0200

    fix(styles): change configuration in tailwind config, index.css to fit personalised theme, finally works!

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router';
-
+import './index.css';
 import Navbar from './components/Navbar';
 import Router from './router/Router';
 
 
 function App() {
-
+ 
   return (
     <BrowserRouter>
       <Navbar />
