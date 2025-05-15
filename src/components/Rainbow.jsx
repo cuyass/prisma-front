a955f459675e18717deb9f8a03fcdef0da2d97b1
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-15 18:56:46 +0200

    feat(rainbowbar): create a rainbow bar to make it more gay, thank you

---
+++ src/components/Rainbow.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const RainbowBar = () => {
+    return (
+        <div className="h-1 bg-gradient-to-r from-red-500 via-yellow-500 to-purple-600"></div>
+    );
+};
+export default RainbowBar;
