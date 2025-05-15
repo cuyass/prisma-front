db093cdeb02445d72e5c48b6671a3d45deea01cd
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-15 20:18:05 +0200

    feat(pagehome): add icon and featured sections, need to style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import HeroSection from "../components/sections/Hero";
+import IconSection from "../components/sections/IconSection";
+import FeaturedContent from "../components/sections/FeaturedSection";
 
 function Home() {
   return (
     <div>
       <HeroSection />
+      <IconSection />
+      <FeaturedContent />
     </div>
   );
 }
