7755ecf4199bc877d297187b7d156975ea435dec
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-15 18:01:24 +0200

    style(navbar): add style in font in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => { 
     return (
-        <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar bg-primary shadow-sm">
         <div className="navbar-start">
             
         </div>
         <div className="navbar-center">
-            <a className="btn btn-ghost text-xl">PRISMA</a>
+            <a className="font-[Righteous] text-2xl">PRISMA</a>
         </div>
         <div className="navbar-end">
         <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-square px-10">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="square" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
             </div>
             <ul
                 tabIndex={0}
