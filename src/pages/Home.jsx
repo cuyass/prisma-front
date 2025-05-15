1c3d6ff14485e97cb423c822a9242640c0552ff9
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-15 18:30:11 +0200

    hotfix(dev): delete test class in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,7 @@ import HeroSection from "../components/Hero";
 function Home() {
   return (
     <div>
-        <div className="hero bg-base-200 h-full border-2 border-red-500">
-
       <HeroSection />
-      </div>
     </div>
   );
 }
