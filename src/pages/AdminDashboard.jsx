b0dd0a88a1d287c0617818ea7577ef886e42b337
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-17 14:34:17 +0200

    feat(drawer): add drawer label in admin dashboard

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -52,26 +52,50 @@ function AdminDashboard() {
     };
     console.log("Lecciones:", lessons);
     return (
-        <div className="min-h-screen bg-gray-100 p-6">
-            <div className="mb-6">
-                <h1 className="font-[Righteous]">Panell d'administrador</h1>
-            </div>
+        <div className="bg-error text-white p-4 sm:bg-info lg:bg-success">
+  Tailwind test
 
+        <div className="drawer lg:drawer-open">
+          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+    
+          {/* MAIN CONTENT */}
+          <div className="drawer-content flex flex-col p-6">
+            {/* Button to open drawer in mobile */}
+            <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden mb-4">
+              Obrir menú
+            </label>
+    
+            <h1 className="font-[Righteous] text-2xl mb-4">Panell d'administrador</h1>
+    
             {alertVisible && (
-                <Alert variant={alertVariant} duration={3} closable>
-                    {alertMessage}
-                </Alert>
+              <Alert variant={alertVariant} duration={3} closable>
+                {alertMessage}
+              </Alert>
             )}
-
+    
             <div className="overflow-x-auto bg-white rounded-lg shadow-md">
-                <LessonTable
-                    lessons={lessons}
-                    onDelete={handleDelete}
-                    onEdit={handleEdit}
-                    isDeleting={isDeleting} />
+              <LessonTable
+                lessons={lessons}
+                onDelete={handleDelete}
+                onEdit={handleEdit}
+                isDeleting={isDeleting}
+              />
             </div>
+          </div>
+    
+          {/* DRAWER SIDEBAR */}
+          <div className="drawer-side">
+            <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+            <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+              <li><a href="#">Lliçons</a></li>
+              <li><a href="#">Usuaris</a></li>
+              <li><a href="#">Mètriques</a></li>
+              <li><a href="#">Configuració</a></li>
+            </ul>
+          </div>
         </div>
-    );
-};
+        </div>
+      );
+    }
 
 export default AdminDashboard;
