44d27d3bc5a7f4fbbdbffaea855f38ff48a6afc6
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-18 13:14:39 +0200

    hotfix(dev): fix navbar dropdown menu, somehow it lost its style?

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,15 +17,15 @@ const Navbar = () => {
             <HamburgerButton />
             <ul
                 tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-40 p-2 shadow">
-                <li className="font-body"><Link to="/" className="block w-full text-right">Inici</Link></li>
-                <li className="font-body"><Link to="/admindashboard" className="block w-full text-right">Panell d'administrador</Link></li>
-                <li className="font-body"><Link to="/learn" className="block w-full text-right">Guies</Link></li>
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <li><Link to="/" className="font-[Roboto]">Inici</Link></li>
+                <li><Link to="/admindashboard" className="font-[Roboto]">Panell d'administrador</Link></li>
+                <li><Link to="/learn" className="font-[Roboto]">Guies</Link></li>
                 {/* 
-                <li className="font-[Roboto]"><Link to="/FAQ">FAQ</Link></li>
-                <li className="font-[Roboto]"><Link to="/directory">Directori</Link></li>
-                <li className="font-[Roboto]"><Link to="/register">Registrar-se</Link></li>
-                <li className="font-[Roboto]"><Link to="/login">Iniciar sessió</Link></li>
+                <li><Link to="/FAQ" className="font-[Roboto]">FAQ</Link></li>
+                <li><Link to="/directory" className="font-[Roboto]">Directori</Link></li>
+                <li><Link to="/register" className="font-[Roboto]">Registrar-se</Link></li>
+                <li><Link to="/login" className="font-[Roboto]">Iniciar sessió</Link></li>
                  */}
             </ul>
             </div>
