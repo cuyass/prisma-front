3aea5c7d05e893acd006a4a3dd1b9f74709ba6e0
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-18 13:20:11 +0200

    hotfix(dev): fix comented route in router? learn? a route that I already uncomented in another commit? I'm going cucu

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -17,8 +17,9 @@ function Router() {
             <Route path="/admindashboard" element={<AdminDashboard />} />
             <Route path="/edit" element={<MarkdownLessonEditor />} />
             <Route path="/edit/:lessonId" element={<MarkdownLessonEditor />} />
-            {/*
             <Route path="/learn" element={<Learn />} />
+            {/*
+            
 {/*             
             <Route path="/FAQ" element={<FAQ />} />
             <Route path="/directory" element={<Directory />} />
